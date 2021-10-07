8f375e7ffe371200df48750107e213aca2983988
Tony_test <tonypreme@gmail.com>
2021-10-07 10:29:12 +0700

    update 07 10 2021

---
+++ EP6_assignment4.py
@@ -0,0 +1,13 @@
+# เรียงลำดับชื่อ 
+student=["ก้อง","แม้ว","โทนี่","โทนั่น"]
+
+print("ก่อนเรียงลำดับ",student)
+
+# เรียงลำดับจาก ก - ฮ
+student.sort()
+print("ใช้คำสั่ง .sort =>",student)
+
+# สลับด้านการเรียงลำดับ จากคำสั่งด้านบน
+student.reverse()
+print("ใช้คำสั่ง .reverse",student) 
+# ** อ่างอิงมาจาก assignment1
