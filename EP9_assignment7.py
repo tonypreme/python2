79fde072ed4a3c18fd0bd84ddf536aeb8a546323
Tony_test <tonypreme@gmail.com>
2021-10-15 09:43:57 +0700

    update 15 10 2021

--- EP9_assignment7.py
+++ EP9_assignment7.py
@@ -1,1 +1,19 @@
-# จับคู่คำทักทาย 
+# จับคู่คำทักทาย / บุลคล
+
+'''
+แบ่งกลุ่มคำทักทาย ** hi,hello
+กับกลุ่มของบุคคล ** โทนี่,โทนวย
+
+ต้องการ 
+
+hi โทนี่ , hi โทนวย
+'''
+
+greeting=["Hi","Hello","Good bye"]
+people=["โทนี่","โทนวย","โทนาฟ"]
+result=[]
+# แบบปกติ
+for x in greeting:
+    for y in people:
+        result.append(x+y)
+print(result)
