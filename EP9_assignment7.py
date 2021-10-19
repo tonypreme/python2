ea10b337cc5f7a77a5b453377a05d4c87d68d6c4
Tony_test <tonypreme@gmail.com>
2021-10-19 10:11:12 +0700

    update 19 10 2021

--- EP9_assignment7.py
+++ EP9_assignment7.py
@@ -12,8 +12,13 @@ hi โทนี่ , hi โทนวย
 greeting=["Hi","Hello","Good bye"]
 people=["โทนี่","โทนวย","โทนาฟ"]
 result=[]
+
 # แบบปกติ
-for x in greeting:
-    for y in people:
-        result.append(x+y)
+for x in greeting: # หยิบข้อมูลใน greeting วนไป
+    for y in people: # หยิบข้อมูลใน people วนไป
+        result.append(x+y) # ใช้คำสั่ง .append ยัดข้อมูลลงไปใน ตัวแปร result
+print(result)
+
+# แบบย่อ 
+result=[x+y for x in greeting for y in people] # ควรทำความเข้าใจก่อน
 print(result)
