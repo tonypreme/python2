2bbeac433a3a86c6ee9fdb76dbffd08895ac380b
Tony_test <tonypreme@gmail.com>
2022-01-06 09:44:41 +0700

    upadte 06 01 2022

--- EP17_Function.py
+++ EP17_Function.py
@@ -1,1 +1,35 @@
 # ฟังก์ชั่น ( Function )
+
+'''
+https://www.youtube.com/watch?v=2_TK8JYJiwQ&t=18397s&ab_channel=KongRuksiam
+ฟังในนี้เวลา 5:06:40 
+'''
+# สาเหตุที่เราต้องเขียน function
+
+a,b,c,d = 10,23,40,50
+
+# คำสั่งที่มันซ้ำๆ 
+
+if a%2 == 0:
+    print("เลขคู่")
+else :
+    print("เลขคี่")
+
+if b%2 == 0:
+    print("เลขคู่")
+else :
+    print("เลขคี่")
+
+if c%2 == 0:
+    print("เลขคู่")
+else :
+    print("เลขคี่")
+
+if d%2 == 0:
+    print("เลขคู่")
+else :
+    print("เลขคี่")
+
+# ------------------------------------
+
+# การส้ราง function และเรียกใช้งาน
