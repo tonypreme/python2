ed16648f7be2d5f6eb1021bf81ef086c63f88e3b
Tony_test <tonypreme@gmail.com>
2022-01-20 10:10:23 +0700

    update 20 01 2022

---
+++ EP19_Arguments&Parameter.py
@@ -0,0 +1,14 @@
+# aguments = ค่าที่ส่งเข้าไปใน funetion 
+# parameter = ค่าตัวแปรที่รับช้อมูลที่ส่งมาทำงาน 
+
+def myData(a,b,c): 
+    print("ชื่อ = ",a,"นามสกุล = ",b,"อายุ = ",c)
+
+fname="pachara"
+lname="555"
+age=27
+
+myData(fname,lname,age)
+
+# aguments = fname lname age  ** ส่ง
+# parameter = a  b  c         ** รับ 
