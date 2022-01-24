e479e83dfe1dc3ce34ce70395e8e123998933718
Tony_test <tonypreme@gmail.com>
2022-01-24 08:44:42 +0700

    update 24 01 2022

--- EP20_assignment10.py
+++ EP20_assignment10.py
@@ -1,25 +1,34 @@
 # ฟังก์ชั่นเลขคู่ - เลขคี่
 
-a,b,c,d = 10,23,40,50
+# a,b,c,d = 10,23,40,50
 
 # คำสั่งที่มันซ้ำๆ 
 
-if a%2 == 0:
-    print("เลขคู่")
-else :
-    print("เลขคี่")
+# if a%2 == 0:
+#     print("เลขคู่")
+# else :
+#     print("เลขคี่")
 
-if b%2 == 0:
-    print("เลขคู่")
-else :
-    print("เลขคี่")
+# if b%2 == 0:
+#     print("เลขคู่")
+# else :
+#     print("เลขคี่")
 
-if c%2 == 0:
-    print("เลขคู่")
-else :
-    print("เลขคี่")
+# if c%2 == 0:
+#     print("เลขคู่")
+# else :
+#     print("เลขคี่")
 
-if d%2 == 0:
-    print("เลขคู่")
-else :
-    print("เลขคี่")
+# if d%2 == 0:
+#     print("เลขคู่")
+# else :
+#     print("เลขคี่")
+
+def functiontony(x):
+    if x%2 == 0:
+        print("เลขคู่")
+    else :
+        print("เลขคี่")
+
+functiontony(5)
+functiontony(10)
