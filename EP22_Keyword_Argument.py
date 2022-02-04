48e93b43584bccff77f06dfb3b5e8cb7328b9522
Tony_test <tonypreme@gmail.com>
2022-02-04 09:04:20 +0700

    update 04 02 2022

--- EP22_Keyword_Argument.py
+++ EP22_Keyword_Argument.py
@@ -1,1 +1,19 @@
-# Keyword Argument 
+# Keyword Arguments
+
+
+def showitem(fname,lname,city):
+    print('ชื่อ :',fname)
+    print('นามสกุล:',lname)
+    print('จังหวัด :',city)
+
+
+# showitem("pachara","sangarun","samutsakorn")
+# การแสดงผลจะเรียงลำดับ ของตัวแปลที่เรากำหนด 
+'''
+fname = ลำดับแรก
+lname = ลำดับสอง
+city = ลำดับสุดท้าย
+'''
+
+# หากต้องการระบุ โดยไม่ต้องเรียงลำดับ
+showitem(city="ปทุม",lname='ojo',fname='ตูตะตู่ตี้ตู')
