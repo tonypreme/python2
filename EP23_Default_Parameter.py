077aad422e2541186adf921c97a2e542df14f7d8
Tony_test <tonypreme@gmail.com>
2022-02-08 09:24:40 +0700

    update 08 02 2022

--- EP23_Default_Parameter.py
+++ EP23_Default_Parameter.py
@@ -1,1 +1,19 @@
 # Default Parameter
+
+'''
+การกำหนดค่าเริ่มต้น
+*** กรณีเราสร้างตัวแปรในฟังชั่นไว้ ถ้าเราไม่ใส่ค่าลงไป จะเกิด Error
+เราจึงต้องมีการ กำหนดค่าเริ่มต้นไว้ 
+เพื่อถ้าเราไม่ใส่ข้อมูล ตัวแปร ก็จะมีค่าในข้อมูลของมันเองได้ ***
+'''
+
+def Showdata(fname,lname,city="Bangkok"): # city มีการกำหนดค่าเริ่มต้นไว้ 
+    # ถ้าหากไม่ใส่ข้อมูลใน city ก็จะมีค่าเริ่มต้นเป็น Bangkok
+    
+    print("Name :",fname)
+    print("Lastname :",lname)
+    print("City :",city)
+
+
+Showdata(fname="Pachara",lname="Sangarun")
+Showdata(fname="Pachara",lname="Sangarun",city="Samutsakorn")
