5ffd4313f169e99c7ab2066b8790cf118cdb29c8
Tony_test <tonypreme@gmail.com>
2022-02-10 09:30:09 +0700

    update 10 02 2022

--- EP24_List_Parameter.py
+++ EP24_List_Parameter.py
@@ -1,1 +1,12 @@
 # List Parameter
+
+def displayFruit(item): 
+    for i in range(len(item)):
+        print( "ผลไม้ชิ้นที่ " , i + 1 , " = " , item[i] )
+
+
+fruit=["มะม่วง","มะละกอ"] # ชนิดข้อมูลเป็น list
+vet=("ชะอม","ผักกาด","คะน้า") # ชนิดข้อมูลแบบ tuple
+
+# displayFruit(fruit)
+displayFruit(vet)
