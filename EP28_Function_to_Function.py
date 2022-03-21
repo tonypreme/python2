49ef52a05727fd83afb5b3c83ad0c6efdaef5808
Tony_test <tonypreme@gmail.com>
2022-03-21 10:07:42 +0700

    update 21 03 2022

--- EP28_Function_to_Function.py
+++ EP28_Function_to_Function.py
@@ -1,18 +1,21 @@
 # ฟังก์ชั่น เรียก ฟังก์ชั่น
 
-def equal(x,y,z):
-    # ฟังก์ชั่น เรียก ฟังก์ชั่น
-    a=compareMax(x,y) # เทียบตัวเลขรอบแรก
-    m=compareMax(a,z) # เทียบตัวเลขรอบสอง
-    return m
-
-
 def compareMax(x,y):
     if x>y:
         return x
     else:
         return y
 
+
+
+def equal(x,y,z):
+    # ฟังก์ชั่น เรียก ฟังก์ชั่น
+    a=compareMax(x,y) # เทียบตัวเลขรอบแรก
+    m=compareMax(a,z) # เทียบตัวเลขรอบสอง
+    return m
+    
+    # return compareMax(compareMax(x,y),z)
+    
 
 
 
